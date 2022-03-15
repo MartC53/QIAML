bdf1810f54b4ab1d401da59c3df1fff8832add02
Luke Orr <leorr@uw.edu>
2022-03-15 09:54:10 -0700

    adding app into the main branch

---
+++ model.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+
+"""This will load the model and return model converted into probability resutls
+"""
+def loadmodel():
+    
+    model = tf.keras.models.load_model('finalcnn')
+    #model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+    return model
+   
+def predict(img, model):
+    img_resize = img.copy()
+    img_resized = img_resize.resize((900,900))
+    img_resized = (np.expand_dims(img_resized,0))
+    predictions = model.predict(img_resized)
+    return predictions
