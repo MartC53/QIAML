5fb53c6bff154e61e8ca7527947fe2e9298bed2f
Luke Orr <leorr@uw.edu>
2022-03-15 10:25:54 -0700

    Update Env

--- model.py
+++ model.py
@@ -1,15 +1,31 @@
 import tensorflow as tf
 import numpy as np
 
-"""This will load the model and return model converted into probability resutls
-"""
 def loadmodel():
+"""
     
+    Returns
+    -------
+    model : to be used for testing unknown images
+        
+    """
     model = tf.keras.models.load_model('finalcnn')
     #model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
     return model
    
 def predict(img, model):
+ """
+    
+    Parameters
+    ----------
+    img : PIL Image
+    model: the imported tf.cnn model
+    Returns
+    -------
+    predictions : array 
+        The array contains the confidence level for the given catigory
+        
+    """   
     img_resize = img.copy()
     img_resized = img_resize.resize((900,900))
     img_resized = (np.expand_dims(img_resized,0))
